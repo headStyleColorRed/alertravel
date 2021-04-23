b2fda88664f55790ea668ce7068b883af11b8e04
Rodrigo Labrador <headstylecolorred@gmail.com>
2021-04-23 13:37:53 +0200

    Initial Commit

---
+++ alertravel/alertravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  alertravelApp.swift
+//  alertravel
+//
+//  Created by Rodrigo Labrador Serrano on 23/4/21.
+//
+
+import SwiftUI
+
+@main
+struct alertravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
